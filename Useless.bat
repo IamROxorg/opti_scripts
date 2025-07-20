836a92a2220ad01ba5a83a11c68739448fb9e278
IamROxorg <imroxorg@gmail.com>
2025-07-20 09:08:29 +0200

    Update Useless.bat

--- Useless.bat
+++ Useless.bat
@@ -336,3 +336,8 @@ reg add "HKCR\.bat\ShellNew" /v FileName /t REG_SZ /d "C:\Windows\ShellNew\templ
 reg delete "HKCR\.bat\ShellNew" /v NullFile /f >nul 2>&1
 reg add "HKCR\.reg\ShellNew" /v FileName /t REG_SZ /d "C:\Windows\ShellNew\template.reg" /f
 reg delete "HKCR\.reg\ShellNew" /v NullFile /f >nul 2>&1
+
+REM Remove Home & Gallery from Explorer
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}" /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
